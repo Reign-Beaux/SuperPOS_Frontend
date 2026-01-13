f26dab4aabf4d181a9c550695572ad6d31aa1346
Saúl Antonio Morquecho Cela <samc920130@hotmail.com>
2026-01-13 00:46:08 -0600

    se cre layout

--- src/components/layouts/mainLayout/MainLayout.tsx
+++ src/components/layouts/mainLayout/MainLayout.tsx
@@ -1,7 +1,23 @@
+import {
+    SidebarProvider,
+    SidebarTrigger,
+} from "@/components/elements/sidebar";
+import { AppSidebar } from "./AppSidebar";
+import { Outlet } from "react-router-dom";
+
 export const MainLayout = () => {
     return (
-        <div>
-            <h1>Main Layout</h1>
-        </div>
+        <SidebarProvider>
+            <AppSidebar />
+            <main className="w-full">
+                <div className="flex h-16 items-center border-b px-4">
+                    <SidebarTrigger />
+                    <div className="ml-4 font-semibold">SuperPOS</div>
+                </div>
+                <div className="p-4">
+                    <Outlet />
+                </div>
+            </main>
+        </SidebarProvider>
     );
 };
